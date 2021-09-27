53fcff9ed384ccbcb5bb1b32f4453320f7d0a739
Whiplash <kountdown@mail.ru>
2021-09-27 10:29:14 +0300

    Initial commit

---
+++ Task1_State.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+struct state{
+    string first_name;
+    string last_name;
+    string date;
+    int cash;
+};
+
+void add(ofstream& file, state& record) {
+    file << record.first_name << " " << record.last_name << " " << record.date << " " << record.cash << endl;
+}
+
+void read(ifstream& file, state& record) {
+    do {
+        file >> record.first_name >> record.last_name >> record.date >> record.cash;
+        cout << record.first_name << " " << record.last_name << " "  << record.date << " " << record.cash
+        << " " << endl;
+    } while(!(file.eof() || record.first_name.empty()));
+}
+
+int main() {
+    string operation;
+    cout << "Enter a preferable operation:\n";
+    cin >> operation;
+    state record;
+    if (operation == "add") {
+        ofstream file("state.txt", ios::app);
+        cout << "Enter a record:\n";
+        cin >> record.first_name >> record.last_name >> record.date >> record.cash;
+        add(file, record);
+        file.close();
+    } else if (operation == "read") {
+        ifstream file("state.txt");
+        read(file,record);
+        file.close();
+    } else {
+        cout << "Wrong type of operation!";
+    }
+}
